955a748b1860fb5a073a9474fbe62ef68f383892
shigemk2 <i.am.shige@gmail.com>
2016-04-10 20:31:00 +0900

    Add sample

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -1,7 +1,11 @@
 package com.example
 
+import scalaz._
+import std.option._, std.list._
+
 object Hello {
   def main(args: Array[String]): Unit = {
+    println(Apply[Option].apply2(some(1), some(2))((a, b) => a + b))
     println("Hello, world!")
   }
 }
