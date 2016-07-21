801d53f362e207c41b2b88970dc9388fa32fd9d7
shigemk2 <i.am.shige@gmail.com>
2016-07-22 02:16:32 +0900

    Add traverse

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -6,6 +6,7 @@ import std.option._, std.list._
 object Hello {
   def main(args: Array[String]): Unit = {
     println(Apply[Option].apply2(some(1), some(2))((a, b) => a + b))
+    println(Traverse[List].traverse(List(1, 2, 3))(i => some(i)))
     println("Hello, world!")
   }
 }
