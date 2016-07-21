4bea8620087cf7f1f6ca07f1781590f8b8830268
shigemk2 <i.am.shige@gmail.com>
2016-07-22 02:20:06 +0900

    Ops

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -2,11 +2,16 @@ package com.example
 
 import scalaz._
 import std.option._, std.list._
+import std.list._
+import syntax.bind._
 
 object Hello {
   def main(args: Array[String]): Unit = {
     println(Apply[Option].apply2(some(1), some(2))((a, b) => a + b))
     println(Traverse[List].traverse(List(1, 2, 3))(i => some(i)))
+    println(List(List(1)).join)
+    println(List(true, false).ifM(List(0, 1), List(2, 3)))
+    println(List(true, true).ifM(List(0, 1), List(2, 3)))
     println("Hello, world!")
   }
 }
